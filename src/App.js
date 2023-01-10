c3227ca6a23d449421a4d6bef74ec73eb45d04f8
ROXBOZ <hello@roxanne-borloz.net>
2023-01-10 17:32:10 +0100

    started with grid and material ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import GridCharacters from "./components/GridCharacters";
+import { Typography } from "@mui/material";
 
 function App() {
   // let data;
@@ -17,7 +18,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Typography variant="h1">hello</Typography>
+      <Typography gutterBottom>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        voluptatem quisquam aut odit quibusdam officiis voluptas odio in!
+        Accusamus excepturi culpa nobis expedita alias similique harum at
+        aperiam corporis. Distinctio?
+      </Typography>
       <GridCharacters />
     </div>
   );
--- src/components/GridCharacters.js
+++ src/components/GridCharacters.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
+import Dashboard from "./Dashboard";
 
 const GridCharacters = () => {
+  // fetch data
   const [data, setData] = useState([]);
-
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/")
       .then((response) => {
@@ -16,12 +19,33 @@ const GridCharacters = () => {
         console.log("error: ", error);
       });
   }, []);
+  // Handle Modal
+
+  const handleModal = () => {
+    console.log("yolo");
+  };
 
   return (
-    <div>
+    <div className="character-grid-container">
       {data &&
         data.map((d) => {
-          return <p>{d.name}</p>;
+          return (
+            <div key={d.id} className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src={d.image} alt={d.name} />
+                </div>
+                <div className="flip-card-back">
+                  <div className="flip-card-back-content">
+                    <Typography>{d.name}</Typography>
+                    <Button variant="contained" onClick={handleModal}>
+                      learn more
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
         })}
     </div>
   );
